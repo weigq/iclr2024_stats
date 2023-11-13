0aedd887de3a2a01176ac87dfc67aa7e15cd0693
weiguoqiang <weiguoqiang.9@bytedance.com>
2023-11-13 16:47:58 +0800

    update sort

--- web_src/stats.js
+++ web_src/stats.js
@@ -1,1 +1,32 @@
-function scrollFunction(){document.body.scrollTop>20||document.documentElement.scrollTop>20?mybutton.style.display="block":mybutton.style.display="none"}function topFunction(){document.body.scrollTop=0,document.documentElement.scrollTop=0}new Tablesort(document.getElementById("all-subs")),mybutton=document.getElementById("myBtn"),window.onscroll=function(){scrollFunction()};
+function scrollFunction(){
+    document.body.scrollTop>20||document.documentElement.scrollTop>20?mybutton.style.display="block":mybutton.style.display="none"
+}
+function topFunction(){
+    document.body.scrollTop=0,document.documentElement.scrollTop=0
+}
+mybutton=document.getElementById("myBtn");
+window.onscroll=function(){scrollFunction()};
+
+$(document).ready(function() {
+    let table = $('#all-subs').dataTable({
+        dom: "<'#searchBox'>frtip",
+        "order": [[ 2, 'des' ]],
+        "columnDefs": [ {
+            "searchable": false,
+            "orderable": false,
+            "targets": 0
+        } ],
+    });
+
+    $(".dataTables_info").appendTo("#customInfo");
+
+    table.api().on('order.dt', function () {
+        table.api().column(0, {order:'applied'}).nodes().each(function (cell, i) {
+            cell.innerHTML = i+1;
+        });
+    }).draw();
+
+    $('#searchBox').keyup(function(){
+        $('#all-subs').DataTable().search($(this).val()).draw() ;
+    });
+});
